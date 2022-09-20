50db5d8875a4b9553ba7dc50d671202afd852cb9
Labert Paixão <labert.ribeiroo@gmail.com>
2022-09-19 21:58:14 -0300

    Estrutura do Projeto

---
+++ ProjectAPS/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProjectAPS project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectAPS.settings')
+
+application = get_asgi_application()
