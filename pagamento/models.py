8053098a9a7b02f1b6a5c20c9e6c5863ed88c758
Labert Paixão <labert.ribeiroo@gmail.com>
2022-11-14 20:47:32 -0300

    Update Models Pagamento

--- pagamento/models.py
+++ pagamento/models.py
@@ -3,7 +3,7 @@ from aluno.models import Aluno
 
 class Pagamento(models.Model):
     nome_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
-    data_pagamento = models.DateField(unique=True)
+    data_pagamento = models.DateField()
 
     def __str__(self) -> str:
         return self.nome_aluno
